4d4e0964209777c036d202c89dccb4783094fea7
edman3d <ejsmajestic@gmail.com>
2023-12-08 12:16:21 -0500

    add get single building endpoint

--- src/controllers/buildings.ts
+++ src/controllers/buildings.ts
@@ -12,3 +12,23 @@ export const getBuildings: RequestHandler = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getBuilding: RequestHandler = async (req, res, next) => {
+  const buildingId = req.params.buildingId;
+
+  try {
+    if (!mongoose.isValidObjectId(buildingId)) {
+      throw createHttpError(400, "Invalid building id");
+    }
+
+    const building = await BuildingModel.findById(buildingId).exec();
+
+    if (!building) {
+      throw createHttpError(404, "Building not found");
+    }
+
+    res.status(200).json(building);
+  } catch (error) {
+    next(error);
+  }
+};
