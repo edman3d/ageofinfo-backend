60cd20e36904ddfaf484b4b83fb2394cb56ce408
edman3d <ejsmajestic@gmail.com>
2023-12-08 12:13:39 -0500

    add get single unit endpoint

--- src/controllers/units.ts
+++ src/controllers/units.ts
@@ -12,3 +12,23 @@ export const getUnits: RequestHandler = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUnit: RequestHandler = async (req, res, next) => {
+  const unitId = req.params.unitId;
+
+  try {
+    if (!mongoose.isValidObjectId(unitId)) {
+      throw createHttpError(400, "Invalid unit id");
+    }
+
+    const unit = await UnitModel.findById(unitId).exec();
+
+    if (!unit) {
+      throw createHttpError(404, "Unit not found");
+    }
+
+    res.status(200).json(unit);
+  } catch (error) {
+    next(error);
+  }
+};
